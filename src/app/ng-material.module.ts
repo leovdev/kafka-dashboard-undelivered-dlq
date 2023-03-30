9cc8ab6c7ae285e7884f9269ff6102872262a094
leonel vinasco <debi@MacBook-Air-de-leonel.local>
2023-03-30 00:21:40 -0500

    Angular material setted

---
+++ src/app/ng-material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core'
+import { MatTableModule } from '@angular/material/table'
+
+@NgModule({
+    declarations:[
+
+    ],
+    imports: [
+        MatTableModule,
+    ],
+    exports: [
+        MatTableModule,
+    ]
+})
+export class NgMaterialModule { }
